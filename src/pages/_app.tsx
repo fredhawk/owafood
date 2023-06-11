0a83e4fcf53c291e46aca74bd096e9cd4736b7b2
Fred Hawk <fredrichawk@gmail.com>
2023-06-11 16:30:28 +0200

    feat: Add auth.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,39 @@
+import {
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton
+} from "@clerk/nextjs";
 import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
 
+function Header() {
+  return (
+    <header
+      style={{ display: "flex", justifyContent: "space-between", padding: 20 }}
+    >
+      <h1>My App</h1>
+      <SignedIn>
+        {/* Mount the UserButton component */}
+        <UserButton afterSignOutUrl="/"/>
+      </SignedIn>
+      <SignedOut>
+        {/* Signed out users get sign in button */}
+        <SignInButton />
+      </SignedOut>
+    </header>
+  );
+}
+
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ClerkProvider {...pageProps}>
+      <Header />
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
